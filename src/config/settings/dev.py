217db30079bbb12e382cfdf3d600b45b75d0a54a
Mykhailo Lazoryk <mikolaz2727@gmail.com>
2025-05-20 21:08:46 +0300

    Changed settings

---
+++ src/config/settings/dev.py
@@ -0,0 +1,18 @@
+from config.settings.base import *
+
+SECRET_KEY = "django-insecure-mp3m1$h1($o)*wekb!t2=y$b^u)16t8gt)m6r$xbyxnx647l(4"
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# MIDDLEWARE + = ['']
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+STATIC_URL = "static/"
---
+++ src/config/settings/prod.py
@@ -0,0 +1,16 @@
+from config.settings.base import *
+
+SECRET_KEY = ""
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+STATIC_URL = "static/"
