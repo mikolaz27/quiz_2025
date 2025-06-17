fde6b7dff7a61147ad51806fbac4e98893d1ff8b
Mykhailo Lazoryk <mikolaz2727@gmail.com>
2025-06-17 21:24:30 +0300

    Added pgadmin and postgres

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -15,6 +15,15 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",  # NOQA:F405
     },
+    # "default_local": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "test_db",
+    #     "USER": "test_user",
+    #     "PASSWORD": "admin",
+    #     "HOST": "localhost",
+    #     "PORT": 5432,
+    # },
+
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB"),
